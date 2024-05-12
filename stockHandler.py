aa268d8f56161823a7cf9398d4794b29fc27ea77
nepstein <nepstein@microsoft.com>
2024-05-12 07:34:25 -0700

    Added Buy mechanism

--- stockHandler.py
+++ stockHandler.py
@@ -3,11 +3,13 @@ from traderHandler import TraderHandler
 
 def run_stock_exchange(trader_handler: TraderHandler, init_stock: int, commission: int, last_day: int):
     current_day = 1
+    deviation = 0.1
+    offers_per_trader = 2
     current_stock_value = init_stock
     
     while current_day <= last_day:
         # buy phase
-        day_buys = trader_handler.activate_buy_phase(current_stock_value, 2, 0.1, commission, [current_day, last_day])
+        day_buys = trader_handler.activate_buy_phase(current_stock_value, offers_per_trader, deviation, commission, [current_day, last_day])
         # sell phase
         day_sells = trader_handler.activate_sell_phase(current_stock_value, commission, [current_day, last_day])
         # adjust stock phase
--- traderHandler.py
+++ traderHandler.py
@@ -1,6 +1,6 @@
 from typing import List
 from Traders import traderBot
-
+import random as rnd
 class TraderHandler:
     def __init__(self, traders_input: List[traderBot.TraderBot], initial_money: int):
         self.traders = []
@@ -11,9 +11,24 @@ class TraderHandler:
                 "stocks": 0
             })
 
-    def activate_buy_phase(self, value: int, offers_per_trader: int, deviation: float, commission:int, time) -> int:
-        # TODO
-        pass
+    def activate_buy_phase(self, value: int, offers_per_trader: int, deviation: float, commission: int, time) -> int:
+        refused_offers = []
+        today_buys = 0
+        
+        for i in range(offers_per_trader):
+            for traderInfo in self.traders:
+                trader: traderBot.TraderBot = traderInfo["trader"]
+                offer_price = self.generate_stock_price(value, deviation)
+                
+                if trader.buy_offer(traderInfo["money"], value, offer_price, time):
+                    traderInfo["money"] -= value + commission
+                    traderInfo["stocks"] += 1
+                    today_buys += 1
+                    print(f"{trader.get_name()} bought a stock at {offer_price}")
+                else:
+                    refused_offers.append(offer_price)
+                    print(f"{trader.get_name()} refused a stock at {offer_price}")
+        
 
     def activate_sell_phase(self, value: int, commission: int, time ) -> int:
         total_sales = 0
@@ -33,3 +48,7 @@ class TraderHandler:
         for traderInfo in self.traders:
             trader: traderBot.TraderBot = traderInfo["trader"]
             print(f"{trader.get_name()} has {traderInfo['money']}$ and {traderInfo['stocks']} stocks")
+            
+    def generate_stock_price(self, value, deviation):
+        chip = rnd.randint(-int(value * deviation), int(value * deviation))
+        return value + chip
