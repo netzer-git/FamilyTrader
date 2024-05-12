91c5c7aae0cbb4ab418d154f7bc65899a6448de9
nepstein <nepstein@microsoft.com>
2024-05-12 13:36:57 -0700

    Add basic plots

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from Traders import solidTrader
 from traderHandler import TraderHandler
 from stockHandler import run_stock_exchange
 
+import matplotlib.pyplot as plt
+
 INIT_MONEY = 1_000
 INIT_STOCK = 100
 COMMISSION = 1
@@ -20,12 +22,20 @@ traders = [
 
 def main():
     trader_handler = TraderHandler(traders, INIT_MONEY)
-    run_stock_exchange(trader_handler, INIT_STOCK, COMMISSION, LAST_DAY)
+    metadata = run_stock_exchange(trader_handler, INIT_STOCK, COMMISSION, LAST_DAY)
     
     print()
     print("*** Stock exchange simulation ended ***")
     print()
     trader_handler.pretty_print()
+
+    # paint stocks plots
+    fig, ax = plt.subplots()
+    ax.plot(range(1, 11), metadata["stocks_data"],)
+    ax.set(xlabel='Day', ylabel='Price',
+        title='Stock prices over the week')
+    ax.grid()
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- stockHandler.py
+++ stockHandler.py
@@ -8,6 +8,9 @@ def run_stock_exchange(trader_handler: TraderHandler, init_stock: int, commissio
     current_stock_value = init_stock
     stocks_in_market = 0
     
+    # review data objects
+    stocks_data = []
+    
     while current_day <= last_day:
         # buy phase
         day_buys = trader_handler.activate_buy_phase(current_stock_value, offers_per_trader, deviation, commission, [current_day, last_day])
@@ -15,18 +18,21 @@ def run_stock_exchange(trader_handler: TraderHandler, init_stock: int, commissio
         day_sells = trader_handler.activate_sell_phase(current_stock_value, commission, [current_day, last_day])
         # adjust stock phase
         stocks_in_market += day_buys - day_sells
+        stocks_data.append(current_stock_value)
         current_stock_value = adjust_stock_value(current_stock_value, day_buys, day_sells, deviation, stocks_in_market)
         # day advance
         print(f"Day {current_day} ended. Stock value: {current_stock_value}. Stocks in market: {stocks_in_market}")
         print()
         current_day += 1
+        
+    return { "stocks_data": stocks_data }
 
 def adjust_stock_value(value: int, day_buys: int, day_sells: int, deviation: float, stocks_in_market: int) -> int:
     # raise the value in % by the difference between buys and sells
     if stocks_in_market == 0:
         stocks_in_market = 1
 
-    impact = (day_buys - day_sells) * (value * 0.1) * (1 / stocks_in_market)
+    impact = int((day_buys - day_sells) * (value * 0.1) * (1 / stocks_in_market))
     # add a random deviation
     chip = rnd.randint(-int(value * deviation), int(value * deviation))
     return value + chip + impact
