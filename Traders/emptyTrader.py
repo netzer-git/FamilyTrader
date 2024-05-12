b20fd47e82759fffc4d89bd79dee3ff8e573fbd8
nepstein <nepstein@microsoft.com>
2024-05-12 08:01:57 -0700

    rename traders

--- Traders/emptyTrader.py
+++ Traders/emptyTrader.py
@@ -1,10 +1,12 @@
 from Traders import traderBot
 
 class EmptyTrader(traderBot.TraderBot):
-    _name = "empty"
+    _name = "netzer_empty"
     
     def buy_offer(self, money: int, value: int, price: int, time) -> bool:
+        # Empty trader does not buy
         return False    
     
     def sell_stocks(self, money: int, stocks: int, value: int, time) -> int:
+        # Empty trader does not have stocks to sell
         return 0
--- Traders/randomTrader.py
+++ Traders/randomTrader.py
@@ -1,10 +1,19 @@
 from Traders import traderBot
+import random as rnd
 
 class RandomTrader(traderBot.TraderBot):
-    _name = None
+    _name = "netzer_random"
     
     def buy_offer(self, money: int, value: int, price: int, time) -> bool:
-        raise ValueError("sell_stocks should override")
+        # flip a coin to decide
+        return rnd.choice([True, False])
     
-    def buy_offer(self, money: int, value: int, price: int, time) -> bool:
-        raise ValueError("sell_stocks should override")
+    def sell_stocks(self, money: int, stocks: int, value: int, time) -> int:
+        # sell a coin to decide or on last day sell all
+        if time[0] == time[1]:
+            return stocks
+        elif (rnd.choice([True, False]) and stocks > 0):
+            # if True, random stocks to sell
+            return rnd.randint(1, stocks)
+        else:
+            return 0
